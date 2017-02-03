b87363b2a5d0cd32d45b1c6af7fd8606878c850f
Morty <mortmort5@gmail.com>
2017-02-03 14:45:42 -0700

    Initial Commit

---
+++ GitPractise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitPractise
+//
+//  Created by Sterling Mortensen on 2/3/17.
+//  Copyright © 2017 Sterling Mortensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
